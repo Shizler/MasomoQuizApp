57ffa624671e4418c7bfd4829a98c7e233961059
Shizler <win99nie@gmail.com>
2017-03-16 12:22:41 +0300

    Docopt and quiz_take

---
+++ listsquiz.py
@@ -0,0 +1,17 @@
+import os
+class ListQuiz():
+    def find(self):
+        path='C:\\Users\\andela\\Documents\\venv\\MasomoProject\\quizzes\\'
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file.endswith(".json"):
+                   path_file = os.path.join(root,file)
+                   files_got=file[:5]
+                   print (files_got)
+
+#  q=ListQuiz()
+                  
+# q.find()
+
+
+
--- reading_json.py
+++ reading_json.py
@@ -1,33 +1,66 @@
+
 import json
+import time 
 
-def read_file():
+def read_file(): 
 
-    path ='H:\AndelaBootCampWk2\Masomo\Quiz1.json'
-    json_data=open(path).read()
+    # accesses the json file
+    # start=time.time()
+    path ='C:\\Users\\andela\\Documents\\MasomoProject\\quiz.json'
+    # add var of quiz at the end of path
+    json_data=open(path).read() 
 
-    data=json.loads(json_data)
-
-    for index, question in enumerate(data):
-        print (index+1, question['question'])
-        print ("A:" +question['A'])
-        print ("B:"+ question['B'])
-        print ("C:"+ question['C'])
-        print ("D:"+ question['D'])
-        print ('\n')
-
-        
-    # for element in data['one']:
-    #     for inner_element in element['number']:
-    #         print ("Question" + (inner_element) +":")
-    #     for elements in element['A']:
-    #       print ("A" + (elements))
-    
-
-            
-
-            
-
-
-    
  
-read_file()
+    data=json.loads(json_data) 
+
+    # initializes the var answerlist that stores the score
+
+    answersList = 0
+ 
+    for index, question in enumerate(data): 
+        print (index+1, question['question']) 
+        print ("A:"+ question['A']) 
+        print ("B:"+ question['B']) 
+        print ("C:"+ question['C']) 
+        print ("D:"+ question['D']) 
+        # print ('\n') 
+      
+        answers=input('Enter your answer')
+        if answers.isalpha():
+        # print ('\n') 
+
+          if question['answer'] == answers:
+             answersList += 1
+             print ("Correct Answer")
+          else:
+             print ("Incorrect answer.The correct answer is " + question['answer'])
+             print ('\n') 
+        else:
+            print ("Invalid input.Please input A,B,C or D")
+
+
+    
+    print("You got {} answers right".format(answersList))
+
+    finish=time.time()
+    # if finish-start>20
+    
+    # for element in range(len(data)):
+    #     for key,value in data[element].items():
+    #         print (key[0])
+
+ 
+         
+    # for element in data['one']:      #     for inner_element in element['number']: 
+     #         print ("Question" + (inner_element) +":") 
+     #     for elements in element['A']: 
+    #       print ("A" + (elements)) 
+read_file() 
+ 
+
+
+
+
+
+
+ 
